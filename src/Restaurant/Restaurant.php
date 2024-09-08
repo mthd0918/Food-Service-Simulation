1ac9850138803152faec0c8f19b841e7278cd6ca
Hidechika Matayoshi <hmtys0918@icloud.com>
2024-09-09 01:24:40 +0900

    微修正

--- src/Restaurant/Restaurant.php
+++ src/Restaurant/Restaurant.php
@@ -6,6 +6,11 @@ use FoodOrders\FoodOrder;
 use Persons\Employees\Employee;
 
 class Restaurant {
-    private FoodOrder $menu = [];
-    private Employee $employees = []; 
+    private FoodOrder $menu;
+    private Employee $employees; 
+
+    public function __construct(FoodOrder $menu, Employee $employees) {
+        $this->menu = $menu;
+        $this->employees = $employees;
+    }
 }
